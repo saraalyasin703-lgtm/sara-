696f36122f6a2b8e4473d0fc1bc558527ab63e0d
aljarrahaljarrrah-prog <aljarrahaljarrrah@gmail.com>
2025-12-30 22:47:07 +0300

    sabaa

--- Assets/_Project/GameOverManager.cs
+++ Assets/_Project/GameOverManager.cs
@@ -1,27 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using TMPro;
 
 public class GameOverManager : MonoBehaviour
 {
-    // هذا المربع سنربط فيه نص النتيجة داخل Unity
-    public TextMeshProUGUI finalScoreText;
-
-    void Start()
-    {
-        // استرجاع النتيجة التي خزنها اللاعب عند الموت
-        int score = PlayerPrefs.GetInt("FinalScore", 0);
-        finalScoreText.text = "Final Score: " + score;
-    }
-
-    // وظيفة زر إعادة اللعب
     public void RestartGame()
     {
         SceneManager.LoadScene("GameScene");
     }
 
-    // وظيفة زر القائمة الرئيسية
-    public void BackToMenu()
+    public void GoToMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
